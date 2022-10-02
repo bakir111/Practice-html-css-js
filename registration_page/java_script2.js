79f85cbf0622780e544ab8f46f62752f8296c6a2
bazarkulbakir <bazarkulbakir677@gmail.com>
2022-10-02 11:44:45 +0300

    js exported imported the exlog variable

--- registration_page/java_script2.js
+++ registration_page/java_script2.js
@@ -1,40 +1,45 @@
 
-  function f1(a){
-    if(a.length < 4){
+  function f1(a)
+  {
+    if(a.length < 4)
         return "не мение 4 символа";
-    }
-    else{
+     else
         return "";
-    }
-   
  }
-
-  function f2(b, v){
-    if(b.length != v.length){
+  function f2(b, v)
+  {
+    if(b.length != v.length)
         return  "пароли должны совпадать";
-    }
-     else{
+     else
          return "";
- }
- }
- let rep, psw;
-document.querySelector(".btn_fields").addEventListener("click", function(){
-   let logg = document.querySelector("#login");
- document.querySelector(".error").innerHTML = f1(logg.value);
-});
-
-document.querySelector(".btn_fields").addEventListener("click", function(){
-     psw = document.querySelector("#password1");
-  document.querySelector(".password1_error").innerHTML = f1(psw.value);
- });
-
- document.querySelector(".btn_fields").addEventListener("click", function(){
-     rep = document.querySelector("#repeat_the_password");
-  document.querySelector(".repeat_the_password_error").innerHTML = f1(rep.value);
- });
-
- document.querySelector(".btn_fields").addEventListener("click", function(){ 
-  document.querySelector(".repeat_the_password_error").innerHTML = f2(rep.value, psw.value);
- });
+  }
+ let exlog,
+       rep,
+       psw;
  
- export {f1 ,f2};
+document.querySelector(".btn_fields").addEventListener("click", function()
+   {
+     let logg = document.querySelector("#login");
+     document.querySelector(".error").innerHTML = f1(logg.value);
+     exlog = logg;
+   });
+
+document.querySelector(".btn_fields").addEventListener("click", function()
+   {
+     psw = document.querySelector("#password1");
+     document.querySelector(".password1_error").innerHTML = f1(psw.value);
+   });
+
+document.querySelector(".btn_fields").addEventListener("click", function()
+   {
+     rep = document.querySelector("#repeat_the_password");
+     document.querySelector(".repeat_the_password_error").innerHTML = f1(rep.value);
+   });
+
+document.querySelector(".btn_fields").addEventListener("click", function()
+  { 
+    document.querySelector(".repeat_the_password_error").innerHTML = f2(rep.value, psw.value);
+  });
+export default exlog;
+ 
+
